e0e1d5b76c0a3821f8f96ad8d8473bb94f0ed6bb
aaaabye <2530785465@qq.com>
2022-11-06 22:32:51 +0800

    initial

---
+++ index.js
@@ -0,0 +1,139 @@
+const name=window.opener['name'];
+alert("欢迎"+name+"使用笔记本");
+
+//侧边导航条
+document.querySelector("#home").onclick = function () {
+    document.querySelector(".nav").style.width = "250px";
+    document.querySelector(".nav span").style.display = "block";
+    document.querySelector(".nav img").style.display = "inline-block";
+    document.querySelector(".addNote").style.display="inline-block";
+    document.querySelector(".delNote").style.display="inline-block";
+    document.querySelector(".noteList").style.display="block";
+}
+
+document.querySelector(".nav img").onclick = function () {
+    document.querySelector(".nav").style.width = "0px";
+    document.querySelector(".nav span").style.display = "none";
+    document.querySelector(".nav img").style.display = "none";
+    document.querySelector(".addNote").style.display="none";
+    document.querySelector(".delNote").style.display="none";
+    document.querySelector(".noteList").style.display="none";
+}
+
+// 鼠标移动到textarea上改变边框宽度
+function changeWidth(x) {
+    x.borderWidth = '5px';
+}
+
+function backChange(x) {
+    x.borderWidth = '3px';
+}
+
+//定义要添加的html代码
+const same = '<div class="content"><div class="input"  onclick="edit(this.parentNode.children[0])" onmouseover="changeWidth(this.style)" onmouseout="backChange(this.style)" contenteditable="false"></div><img src="./picture/up.png" alt="" class="up" title="向上移" onclick="moveUp(this.parentNode)"><img src="./picture/down.png" alt="" class="down" title="向下移" onclick="moveDown(this.parentNode)"><img src="./picture/up_down.png" alt="" class="up_jia" title="向上加" onclick="addUp(this.parentNode)"><img src="./picture/down_jia.png" alt="" class="down_jia" title="向下加" onclick="addDown(this.parentNode)"><img src="./picture/minus.png" alt="" class="minus" title="删除" onclick="deleteAll(this.parentNode)"><img src="./picture/view.png" alt="" class="view" title="预览" onclick="view(this.parentNode.children[0])"><img src="./picture/update.png" alt="" class="update" title="修改" onclick="update(this.parentNode.children[0])"></div>';
+
+//输入框序号
+number();
+function number() {
+    let boxs = document.querySelectorAll(".content");
+    for (let i = 0; i < boxs.length; i++) {
+        boxs[i].onclick = function () {
+            let d = new Date();
+            let time = d.toLocaleString();
+            for (let j = 0; j < boxs.length; j++) {
+                if (this == boxs[j]) {
+                    document.getElementById("showNum").innerHTML = "<span>笔记属性：</span><br><br><span>更新时间：" + time + "</span><br><br><span id='name'>作者：</span><br><br><span>单元格属性:<span><br><span>编号：</span>" + j;
+                    console.log(name);
+                    document.getElementById("name").innerText="作者："+name;
+                }
+            }
+        }
+    }
+}
+
+//向上移
+function moveUp(x) {
+    let temp = x.children[0].innerHTML;
+    x.children[0].innerHTML = x.previousSibling.children[0].innerHTML;
+    x.previousSibling.children[0].innerHTML = temp;
+}
+
+//向下移
+function moveDown(x) {
+    let temp = x.children[0].innerHTML;
+    x.children[0].innerHTML = x.nextSibling.children[0].innerHTML;
+    x.nextSibling.children[0].innerHTML = temp;
+}
+
+//向上加
+function addUp(node) {
+    node.insertAdjacentHTML('beforeBegin', same);
+    number();
+    edit();
+}
+
+//向下加
+function addDown(node) {
+    node.insertAdjacentHTML('afterend', same);
+    number();
+    edit();
+}
+
+//删除
+function deleteAll(node) {
+    node.remove();
+    number();
+}
+
+//选中编辑
+function edit(x) {
+    console.log(x);
+    x.setAttribute('contenteditable', 'true');
+}
+
+//预览
+function view(x){
+    x.innerHTML=marked.parse(x.innerText);
+}
+
+//修改
+function update(x){
+    const turndownService = new TurndownService();
+    x.innerText = turndownService.turndown(x.innerHTML);
+}
+
+//使用说明
+function kan(){
+    document.querySelector(".explanation").style.display='block';
+}
+
+function chadiao(){
+    document.querySelector(".explanation").style.display='none';
+}
+
+//新增笔记本
+function addBook(){
+    let nodeli=document.createElement("li");
+    let nodetext=document.createTextNode("新笔记本");
+    nodeli.appendChild(nodetext);
+    document.getElementById("list").appendChild(nodeli);
+    console.log("你好");
+}
+
+//删除笔记本
+function delBook(){
+    let a=document.getElementById("list");
+    a.lastElementChild.remove();
+    console.log("我好");
+}
+
+//提示
+function tishi(){
+    alert("右下角见使用说明");
+}
+tishi();
+
+
+
+
+
---
+++ register.js
@@ -0,0 +1,74 @@
+document.querySelector(".register").addEventListener("click", change);
+function change() {
+    let x = document.querySelector(".push");
+    // x.insertAdjacentHTML('beforeBegin', add);
+    document.querySelector(".three").style.display = "block";
+    document.querySelector(".title span").innerText = "注册";
+    document.querySelector(".id").value = "";
+    document.querySelector(".password").value = "";
+    x.children[1].addEventListener("click", back);
+    x.children[1].removeEventListener("click", change);
+}
+
+function back() {
+    let x1 = document.querySelector(".id").value;
+    let x2 = document.querySelector(".password").value;
+    let x3 = document.querySelector(".name").value;
+
+
+    const myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    let requestOptions = {
+        method: "post",
+        headers: myHeaders,
+        redirect: "follow",
+    }
+    const userdata = {
+        id: x1,
+        password: x2,
+        name: x3,
+    };
+    requestOptions.body = JSON.stringify(userdata);
+    fetch(`${"http://123.60.44.50:3000"}/${"users"}`, requestOptions)
+        .then(response => response.json())
+        .then(data => alert("注册成功"))
+        .catch(err => console.log(err));
+
+
+    document.querySelector(".title span").innerText = "登录";
+    document.querySelector(".register").
+        addEventListener("click", change);
+    document.querySelector(".three").style.display = "none";
+    document.querySelector(".id").value = "";
+    document.querySelector(".password").value = "";
+    document.querySelector(".register").removeEventListener("click", back);
+}
+
+function join() {
+    let x1 = document.querySelector(".id").value;
+    let x2=document.querySelector(".password").value;
+
+    const myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+    let requestOptions = { 
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+    };
+    fetch(`${"http://123.60.44.50:3000"}/${"users"}/${x1}`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data.password);
+            if (data.id == x1 && data.password == x2) {   // 验证是否存在该用户return data
+            alert("登录成功");
+            console.log(data.name);
+            window['name'] = data.name;
+            window.open("index.html");
+            } else {
+                // throw new Error("用户名不存在")
+                alert("用户名不存在或密码错误");
+            }
+        })
+        .catch(err => console.log(err))
+
+}
